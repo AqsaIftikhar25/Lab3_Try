db3541602dce99ec60979e0f2e77558aed70c206
Aqsa Iftikhar <aqsaciit@gmail.com>
2020-09-15 01:34:11 +0200

    Testsuites definition

--- R/myfuncdj.R
+++ R/myfuncdj.R
@@ -5,7 +5,6 @@
 #' @param v1 A number.
 #' @param v2 A number.
 #' @param c A number.
-#'
 #' @references \url{https://en.wikipedia.org/wiki/Dijkstra%27s_algorithm}
 
 
@@ -15,6 +14,8 @@ wiki_graph <-
              v2=c(2,3,6,1,3,4,1,2,4,6,2,3,5,4,6,1,3,5),
              w=c(7,9,14,7,10,15,9,10,11,2,15,11,6,6,9,14,2,9))
 
-# usethis::use_data('wiki_graph')
-save(wiki_graph, file = "data/myfunctdj.Rdata")
+usethis::use_data(wiki_graph, internal = TRUE, overwrite = TRUE)
+# save(wiki_graph, file = "data/myfunctdj.RData")
+# save(wiki_graph, file = "data/myfunctdj.rda")
+
 # usethis::use_data('wiki_graph', pkg = 'Lab3trypackg', internal = TRUE)
